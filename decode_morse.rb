e878e541e94a07a226dd35d371f0f6eb0dead07f
Daniel <carreraprogrammer@gmail.com>
2023-06-15 17:56:12 -0500

    Add method to decode word

--- decode_morse.rb
+++ decode_morse.rb
@@ -5,7 +5,7 @@ $morse_alphabet = {".-" => "A", "-..." => "B", "-.-." => "C", "-.." => "D", "." 
 def decode_letter(code)
   if code.is_a?(String)
     if $morse_alphabet[code] != nil
-      puts $morse_alphabet[code]
+      print $morse_alphabet[code]
     else
       puts "Please introduce a valid morse character"
     end
@@ -14,4 +14,17 @@ def decode_letter(code)
   end
 end
 
-decode_letter(".-")
+
+# Create a method to decode an entire word in Morse code, takes a string parameter, and return the string representation. Every character in a word will be separated by a single space
+
+def decode_word(code)
+  array = code.split(" ")
+  word = ""
+  array.each do |c|
+    letter = decode_letter(c)
+    word.concat(letter) unless letter.nil?
+  end
+  puts word
+end
+
+decode_word("-- -.--")  # => "MY"
