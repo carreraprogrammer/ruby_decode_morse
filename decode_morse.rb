eeabf9ed047e401dd2af2ae0b39b99ca91feef8f
Daniel <carreraprogrammer@gmail.com>
2023-06-15 18:35:37 -0500

    Fix linters

--- decode_morse.rb
+++ decode_morse.rb
@@ -1,25 +1,29 @@
-# Create a method to decode a Morse code character, takes a string parameter, and return the corresponding character in uppercase
-
-$morse_alphabet = {".-" => "A", "-..." => "B", "-.-." => "C", "-.." => "D", "." => "E", "..-." => "F", "--." => "G", "...." => "H", ".." => "I", ".---" => "J", "-.-" => "K",".-.." => "L", "--" => "M", "-." => "N", "---" => "O", ".--." => "P", "--.-" => "Q", ".-." => "R", "..." => "S", "-" => "T", "..-" => "U", "...-" => "V", ".--" => "W", "-..-" => "X", "-.--" => "Y", "--.." => "Z"}
+# Create a method to decode a Morse code character
 
 def decode_letter(code)
+  morse_alphabet = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G',
+    '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N',
+    '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U',
+    '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z'
+  }
+
   if code.is_a?(String)
-    if $morse_alphabet[code] != nil
-      print $morse_alphabet[code]
+    if !morse_alphabet[code].nil?
+      print morse_alphabet[code]
     else
-      puts "Please introduce a valid morse character"
+      puts 'Please introduce a valid morse character'
     end
   else
-    puts "Please, introduce a string"
+    puts 'Please, introduce a string'
   end
 end
 
-
-# Create a method to decode an entire word in Morse code, takes a string parameter, and return the string representation. Every character in a word will be separated by a single space
+# Create a method to decode an entire word in Morse code
 
 def decode_word(code)
-  array = code.split(" ")
-  word = ""
+  array = code.split(' ')
+  word = ''
   array.each do |c|
     letter = decode_letter(c)
     word.concat(letter) unless letter.nil?
@@ -27,4 +31,4 @@ def decode_word(code)
   puts word
 end
 
-decode_word("-- -.--")  # => "MY"
+decode_word('-- -.--') # => "MY"
