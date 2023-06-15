86a9346a782e285a2ba2e42e2953bb799b4faec9
Daniel <carreraprogrammer@gmail.com>
2023-06-15 18:58:39 -0500

    Fix linters

--- decode_morse.rb
+++ decode_morse.rb
@@ -1,45 +1,44 @@
 # Create a method to decode a Morse code character
 
 def decode_letter(code)
-    morse_alphabet = {
-      '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G',
-      '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N',
-      '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U',
-      '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z'
-    }
-  
-    if code.is_a?(String)
-      if morse_alphabet.key?(code)
-        morse_alphabet[code]
-      else
-        puts 'Please introduce a valid morse character'
-      end
+  morse_alphabet = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G',
+    '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N',
+    '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U',
+    '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z'
+  }
+
+  if code.is_a?(String)
+    if morse_alphabet.key?(code)
+      morse_alphabet[code]
     else
-      puts 'Please, introduce a string'
+      puts 'Please introduce a valid morse character'
     end
+  else
+    puts 'Please, introduce a string'
   end
-  
-  # Create a method to decode an entire word in Morse code
-  
-  def decode_word(code)
-    array = code.split(' ')
-    word = ''
-    array.each do |c|
-      letter = decode_letter(c)
-      word.concat(letter) unless letter.nil?
-    end
-    word
+end
+
+# Create a method to decode an entire word in Morse code
+
+def decode_word(code)
+  array = code.split(' ')
+  word = ''
+  array.each do |c|
+    letter = decode_letter(c)
+    word.concat(letter) unless letter.nil?
   end
-  
-  # Create a method to decode the entire message in Morse code
-  
-  def decode_message(code)
-    words = code.split('   ').map do |c|
-      decode_word(c)
-    end
-    message = words.join(' ')
-    puts message
+  word
+end
+
+# Create a method to decode the entire message in Morse code
+
+def decode_message(code)
+  words = code.split('   ').map do |c|
+    decode_word(c)
   end
-  
-  decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...') #=> "A BOX FULL OF RUBIES"
-  
+  message = words.join(' ')
+  puts message
+end
+
+decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...') #=> "A BOX FULL OF RUBIES"
